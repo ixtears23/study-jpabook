daea4e699409c379483cfeb8f581485387624b1b
junseokoh0 <junseok@bodycodi.com>
2023-01-18 22:54:15 +0900

    엔티티 클래스 개발

---
+++ src/main/java/junseok/snr/study/jpabook/jpashop/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package junseok.snr.study.jpabook.jpashop.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
---
+++ src/main/java/junseok/snr/study/jpabook/jpashop/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package junseok.snr.study.jpabook.jpashop.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
