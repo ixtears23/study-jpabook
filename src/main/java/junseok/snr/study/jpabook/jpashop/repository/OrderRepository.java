e259bb28b1db0450db84c8383a97875b3d30d539
junseokoh0 <junseok@bodycodi.com>
2023-02-09 19:23:09 +0900

    OrderRepository 추가

---
+++ src/main/java/junseok/snr/study/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,24 @@
+package junseok.snr.study.jpabook.jpashop.repository;
+
+import junseok.snr.study.jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+}
